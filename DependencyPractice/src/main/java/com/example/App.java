3d83e0ffad27f60b070decb1df0d6ad1eb80b66f
WINDOWS 11 <https://github.com/@SandiFariz/modul-5>
2024-10-24 22:29:52 +0700

    sandi terakhir

--- DependencyPractice/src/main/java/com/example/App.java
+++ DependencyPractice/src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         String text = "Hello, Maven!";
         String reversedText = StringUtils.reverse(text);
